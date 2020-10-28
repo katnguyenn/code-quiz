0b5a173276985a5e100e221f114c52f3f3448c8f
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-10-28 16:00:36 -0700

    Added quiz questions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Variables
-var container = document.getElementsByClassName("container");
+var container = document.getElementById("container");
 var startQuiz = document.getElementById("start-quiz");
 var startButton = document.getElementById("start-button");
 var quizContainer = document.getElementById("quiz");
@@ -8,14 +8,66 @@ var quizQuestions = document.getElementById("questions");
 var result = document.getElementById("result");
 var endQuiz = document.getElementById("endQuiz");
 var finalScore = document.getElementById("finalScore");
-var submitScore = dcoument.getElementById("submitScore");
-var highScore = document.getElementsById("highScore");
+var submitScore = document.getElementById("submitScore");
+var highScore = document.getElementById("highScore");
 var endQuizBtns = document.getElementById("endQuizBtns");
 var playAgain = document.getElementById("playAgain");
-var clearHighScore = documnt.getElementById("clearHighScore");
+var clearHighScore = document.getElementById("clearHighScore");
 
 var questionNumber = -1;
 var score = 0; 
-var correct = "";
+var correct;
 var timeLeft = 60;
+
+// Quiz Questions
+var myQuestions = [
+    {
+        Question: "How does a for loop start?",
+        Choices: {
+            a: "for (var i = 0; i < 5; i++)",
+            b: "for (var i = 0; i < 5)",
+            c: "for (i <5 ; i++)",
+            d: "for (var i = 0; i <5; i+)"
+
+        },
+        correctAnswer: "a"
+    },
+    {
+        Question: "How can you add a comment in Javascript?",
+        Choices: {
+            a: "//This is a comment",
+            b: "<!--This is a comment-->",
+            c: "'This is a comment"
+        },
+        correctAnswer: "b"
+    },
+    {
+        Question: "Javascript is the same as Java.",
+        Choices: {
+            a: "True",
+            b: "False"
+        },
+        correctAnswer: "b"
+    },
+    {
+        Question: "Which operator is used to assign a value?",
+        Choices: {
+            a: "-",
+            b: "X",
+            c: "=",
+            d: "*"
+        },
+        correctAnswer: "c"
+    },
+    {
+        Question: "How do you call a function named myFunction()?",
+        Choices: {
+            a: "call myFunction()",
+            b: "myFunction()",
+            c: "call function myFunction()"
+        },
+        correctAnswer: "b"
+    }
+];
+
 
